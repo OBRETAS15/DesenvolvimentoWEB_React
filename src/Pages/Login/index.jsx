80176a941928aaa2d6390fe523d528241822d2ab
OBRETAS15 <juniorbretasaraujo305@gmail.com>
2024-11-01 11:41:16 -0300

    Inicio do login

---
+++ src/Pages/Login/index.jsx
@@ -0,0 +1,25 @@
+import { Button } from "../../Components/Button"
+import { InputField } from "../../Components/InputField";
+import { Container, Tittle, ContentForms } from "./style";
+
+export const Login = () => {
+
+    const handleLogin = () => {
+    alert("Login");
+    };
+
+    return (
+    <Container>
+        <ContentForms>
+        <Tittle>Login</Tittle>
+
+        <InputField propsplaceholder="Digite seu login" propstype="email" />
+
+        <InputField propsplaceholder="Digite sua senha" propstype="password" />
+
+
+        <Button title="Entrar" handleFunction={handleLogin} />
+        </ContentForms>
+    </Container>
+    );
+};
